3304f839e423d907debf9091aaf1a4a280a4cedb
NaimDanial15 <2021480736@studnent.uitm.edu.my>
2025-07-09 08:57:59 +0800

    updated code for autism center

--- server/api/therapyService/delete.js
+++ server/api/therapyService/delete.js
@@ -1,6 +1,6 @@
 import { promises as fs } from 'fs'
-import path from 'path'
-import prisma from "@@/server/config/prisma-client"
+//import path from 'path'
+//import prisma from "@@/server/config/prisma-client"
 
 export default defineEventHandler(async (event) => {
   if (event.method !== 'DELETE') {
